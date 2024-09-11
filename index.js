5287a102ba53482e2eaf62d1cc65a2dd1caed7be
“[zoni <[khanzoni361@gmail.com]>
2024-09-11 11:50:14 +0500

    Initial commit

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ var form = document.getElementById("form");
 var Output = document.getElementById("resumecontent");
 var resumeOutput = document.getElementById("resumeoutput");
 var side = document.getElementById("side");
+var editResume = document.getElementById("editResume");
 form === null || form === void 0 ? void 0 : form.addEventListener('submit', function (event) {
     event.preventDefault();
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var contact = document.getElementById("phone").value;
     var skill = document.getElementById("skill").value;
+    ;
     var experience = document.getElementById("experience").value;
-    var Content = "\n     \n    \n    <h3>Name: ".concat(name, "</h3>\n    <h3>Email: ").concat(email, "</h3>\n     <h3>Contact No: ").concat(contact, "</h3>\n      <h3>Skills: ").concat(skill, "</h3>\n       <h3>Experience: ").concat(experience, "</h3>\n    ");
+    var Content = "\n     \n    <img src=\"images/success.png\" alt=\"\">\n    <h3 contenteditable=\"true\">Name: ".concat(name, "</h3>\n    <h3 contenteditable=\"true\">Email: ").concat(email, "</h3>\n     <h3 contenteditable=\"true\">Contact No: ").concat(contact, "</h3>\n      <h3 contenteditable=\"true\">Skills: ").concat(skill, "</h3>\n       <h3 contenteditable=\"true\">Experience: ").concat(experience, "</h3>\n    ");
     Output.innerHTML = Content;
-    side.style.width = "50%";
-    resumeOutput.style.display = "block";
+    if (side)
+        side.style.width = "40%";
+    if (resumeOutput)
+        resumeOutput.style.display = "block";
+});
+editResume === null || editResume === void 0 ? void 0 : editResume.addEventListener('click', function () {
+    if (side)
+        side.style.width = "100%";
+    if (resumeOutput)
+        resumeOutput.style.display = "none";
 });
--- index.ts
+++ index.ts
@@ -1,29 +1,39 @@
-const form = document.getElementById("form") ;
-const Output =  document.getElementById("resumecontent");
-const resumeOutput = document.getElementById("resumeoutput");
-const side = document.getElementById("side");
+const form = document.getElementById("form") as HTMLFormElement;
+const Output = document.getElementById("resumecontent") as HTMLElement;
+const resumeOutput = document.getElementById("resumeoutput") as HTMLElement;
+const side = document.getElementById("side") as HTMLElement;
+const editResume = document.getElementById("editResume") as HTMLButtonElement;
 
-form?.addEventListener('submit',function(event){
+
+
+form?.addEventListener('submit', function (event: Event): void {
     event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const contact = document.getElementById("phone").value;
-    const skill = document.getElementById("skill").value;
-    const experience = document.getElementById("experience").value;
+    const name = (document.getElementById("name") as HTMLInputElement).value;
+    const email = (document.getElementById("email") as HTMLInputElement).value;
+    const contact = (document.getElementById("phone") as HTMLInputElement).value;
+    const skill = (document.getElementById("skill") as HTMLInputElement).value;;
+    const experience = (document.getElementById("experience") as HTMLInputElement).value;
 
     const Content = `
      
-    
-    <h3>Name: ${name}</h3>
-    <h3>Email: ${email}</h3>
-     <h3>Contact No: ${contact}</h3>
-      <h3>Skills: ${skill}</h3>
-       <h3>Experience: ${experience}</h3>
+    <img src="images/success.png" alt="">
+    <h3 contenteditable="true">Name: ${name}</h3>
+    <h3 contenteditable="true">Email: ${email}</h3>
+     <h3 contenteditable="true">Contact No: ${contact}</h3>
+      <h3 contenteditable="true">Skills: ${skill}</h3>
+       <h3 contenteditable="true">Experience: ${experience}</h3>
     `;
 
     Output.innerHTML = Content;
 
-    side.style.width = "50%";
+    if (side) side.style.width = "40%";
 
-    resumeOutput.style.display = "block"
-})
+    if (resumeOutput) resumeOutput.style.display = "block"
+});
+
+editResume?.addEventListener('click', function (): void {
+    if (side) side.style.width = "100%";
+    if (resumeOutput) resumeOutput.style.display = "none";
+});
+
+
